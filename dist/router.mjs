e75f8a10031d385c92b00c41b348e539216bf7ab
Samuel Hornsey <hornseysam498@gmail.com>
2019-11-18 22:44:27 +1100

    refactor router

--- dist/router.mjs
+++ dist/router.mjs
@@ -1,63 +1,139 @@
+// Regex constants
+const PARAMS_REGEX = /[:*](\w+)/g;
+const REPLACE_VARIABLE_REGEXP = "([^/]+)";
+const FOLLOWED_BY_SLASH = "(?:/$|$)";
 class Router {
-    /**
-     * Constructor
-     */
-    constructor() {
-        this.routes = new Map();
+    // Constructor
+    constructor(root) {
+        this.routes = [];
+        this.root = root ? root : window.location.origin;
     }
     /**
-     * Initialise the route
+     * Initialize the router
      */
     init() {
         this.redefineLinks();
-        window.addEventListener('popstate', () => this.onChange());
+        window.addEventListener("popstate", () => this.onChange());
         this.onChange();
     }
     /**
-     * Collects all the router links
+     * Collect links and
+     * add event listeners
      */
     redefineLinks() {
-        let links = document.querySelectorAll('a');
-        links.forEach(link => {
-            if (link.hasAttribute('data-router')) {
-                link.addEventListener('click', event => this.go(event));
-            }
-        });
+        let links = document.querySelectorAll("a[data-router]");
+        for (let link of links) {
+            link.addEventListener("click", event => this.go(event));
+        }
     }
     /**
-     * Function to run on that route
-     * @param route string
+     * When that route is called
+     * @param path string
+     * @param func any
      */
-    on(route, cb) {
-        this.routes.set(route, cb);
+    on(path, handler) {
+        let params = path.match(PARAMS_REGEX);
+        if (params) {
+            params = params.map(param => param.replace(":", ""));
+        }
+        this.routes.push({
+            path,
+            handler,
+            params
+        });
         return this;
     }
     /**
-     * Go to url
-     * @param event any
+     * Set not found handler
+     * @param handler function
      */
-    go(event) {
-        event.preventDefault();
-        window.history.pushState(null, null, event.target.href);
-        this.onChange();
+    notFound(handler) {
+        this._notFound = handler;
+        return this;
+    }
+    /**
+     * Replace parameters regex
+     * @param route Route
+     */
+    replace(route) {
+        const names = [];
+        const regex = new RegExp(route.path.replace(PARAMS_REGEX, (full, name) => {
+            names.push(name);
+            return REPLACE_VARIABLE_REGEXP;
+        }) + FOLLOWED_BY_SLASH);
+        return { regex, names };
+    }
+    /**
+     * Get the parameters from the URL
+     * @param match any
+     * @param names Array<string>
+     */
+    getParams(match, names) {
+        if (names.length === 0)
+            return null;
+        if (!match)
+            return null;
+        return match
+            .splice(1, names.length)
+            .reduce((params, value, index) => {
+            if (params === null)
+                params = {};
+            params[names[index]] = value;
+            return params;
+        }, null);
+    }
+    /**
+     * Find the matching routes
+     * @param path string
+     */
+    findRoutes(path) {
+        return this.routes
+            .map(route => {
+            const { regex, names } = this.replace(route);
+            const match = path.replace(/^\/+/, "/").match(regex);
+            const params = this.getParams(match, names);
+            return match ? { match, route, params } : null;
+        })
+            .filter(m => m);
+    }
+    /**
+     * Get the match URL
+     * @param path string
+     */
+    match(path) {
+        return this.findRoutes(path)[0];
     }
     /**
      * On route change
      */
     onChange() {
         let url = window.location.pathname;
-        let searchParams = new URLSearchParams(window.location.search);
-        let query = {};
-        for (let entry of searchParams.entries()) {
-            let [key, val] = entry;
-            query[key] = val;
+        let query = window.location.search;
+        if (this.root) {
+            url = url.replace(this.root, "");
         }
-        let handler = this.routes.get(url);
-        if (handler) {
-            handler({}, query);
+        const m = this.match(url);
+        if (!m) {
+            return this._notFound(query);
         }
-        return;
+        const { route, params } = m;
+        return route.handler(query, params);
+    }
+    /**
+     * Go to URL
+     * @param event any
+     */
+    go(event) {
+        event.preventDefault();
+        const query = event.target.search;
+        const url = query
+            ? `${this.root}${event.target.pathname}${query}`
+            : `${this.root}${event.target.pathname}`;
+        window.history.pushState(null, null, url);
+        this.onChange();
     }
 }
+
+//# sourceMappingURL=index.js.map
 
 export { Router };
